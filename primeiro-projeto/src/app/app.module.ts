be3d9cbd36406256ec4e6e5e2e16f7289926d3e6
theanaoliveira <carolineana363@gmail.com>
2017-03-28 23:06:57 -0300

    Curso Angular 2 - Modulos

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+import { CursosModule } from "./cursos/cursos.module";
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    CursosModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
