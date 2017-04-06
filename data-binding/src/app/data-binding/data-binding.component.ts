349264b86b13b72b0d89943ba3713ad1d116540d
theanaoliveira <carolineana363@gmail.com>
2017-04-05 22:52:36 -0300

    Events

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles:[
+    `
+      .highlight{
+        background-color: yellow;
+        font-weight: bold
+      }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   url:string = 'https://github.com/theanaoliveira';
   cursoAngular:boolean = true;
   urlImage = 'http://lorempixel.com/400/200/';
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1;    
@@ -17,6 +29,23 @@ export class DataBindingComponent implements OnInit {
 
   getCurtirCurso(){
     return true;
+  }
+
+  clickButton(){
+    alert('click');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    //console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
